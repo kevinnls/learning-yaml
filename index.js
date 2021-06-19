2b7e076a8601af6466c1a2f7e00c0e08c050c91c
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-06-19 20:12:20 +0530

    establish basic functionality using json parsing

--- index.js
+++ index.js
@@ -2,3 +2,30 @@
  * does hello world to check
  * their dev environment */
 console.log("hello, world");
+
+let inputObject
+let outputObject
+let queryString
+
+function updateInputObject(e){
+	try {
+		inputObject = JSON.parse(e.target.value)
+		updateOutputObject()
+	} catch (err) {
+		//console.log(err)
+	}
+}
+function updateQueryString(e){
+	queryString = e.target.value
+	updateOutputObject()
+}
+function updateOutputObject(){
+	if (!!queryString && !!inputObject){
+		try {
+			document.getElementById("output").value = inputObject[queryString]
+		} catch (err) {
+			document.getElementById("output").value = "query Could Not Be REsolved"
+		}
+	}
+}
+
